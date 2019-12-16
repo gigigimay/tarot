36c62da5918ee812a5c12bb6aeac108b46fffb51
gigigimay <mo.maytiya_st@tni.ac.th>
2019-12-16 15:43:12 +0700

    init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <button type="button">List</button>
+                <button type="button">Draw</button>
+            </>
+        )
+    }
+}
+
+export default App
